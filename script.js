d3b354d6298f43818f9b7191ef295fbe765ab1d6
jecicciu <107682023+jecicciu@users.noreply.github.com>
2025-01-13 04:14:53 -0300

    Actualziacion

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-  event.preventDefault();  // Evita que el formulario se envíe realmente
-  alert("Gracias por tu mensaje. Nos pondremos en contacto contigo pronto.");
-  // Puedes agregar código para enviar el formulario al backend si es necesario
+document.querySelector("form").addEventListener("submit", function(event) {
+  if (!document.getElementById("nombre").value || !document.getElementById("email").value || !document.getElementById("mensaje").value) {
+      event.preventDefault();
+      alert("Por favor, completa todos los campos.");
+  }
 });
